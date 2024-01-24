58ff330ae1e6099245a48d492efa098e4454b94e
Ptest <Ptest@lit.msu.ru>
2024-01-24 11:38:13 +0300

    First commit

---
+++ Program1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class mas1
+{
+    public double[] m;
+
+    public mas1(int n)
+    {
+        m = new double[n];
+        for(int i = 0; i < n; i++)
+        {
+            m[i] = (double)int.Parse(Console.ReadLine());
+        }
+    }
+
+    public void print()
+    {
+        foreach(double i in m)
+        {
+            Console.Write($"{i}    ");
+        }
+    }
+
+}
+
+namespace kkk1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            mas1 m = new mas1(int.Parse(Console.ReadLine()));
+            m.print();
+        }
+    }
+}
